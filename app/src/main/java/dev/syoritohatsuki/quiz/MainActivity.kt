b3c024ea42f4d8028fde864c3e104a5fbed8a99f
syorito-hatsuki <enderdron3@gmail.com>
2024-11-01 22:20:54 +0200

    Implemented navigation

--- app/src/main/java/dev/syoritohatsuki/quiz/MainActivity.kt
+++ app/src/main/java/dev/syoritohatsuki/quiz/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dev.syoritohatsuki.quiz.ui.components.NavigationHost
 import dev.syoritohatsuki.quiz.ui.theme.QuizTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuizTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavigationHost(navController, innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuizTheme {
-        Greeting("Android")
     }
 }
